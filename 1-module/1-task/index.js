2c29d3a961dba83abc918ccfdb4e2dc1735301c6
Retover <lehasazonoff@mail.ru>
2021-12-17 22:34:49 +0300

    1-5 task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,19 @@
 function factorial(n) {
   // ваш код...
-}
+  let result = 1;     
+               if (n == 0){
+                   return 1;
+               } else{
+               while(n > 0){
+                   result *= n;
+                   n--;
+               }
+               
+               return result;
+            }
+           }
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5);
+
