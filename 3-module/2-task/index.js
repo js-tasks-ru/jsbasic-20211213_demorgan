a7852bfe7a578c1a29fedce6099d69c3c9b51e36
Retover <lehasazonoff@mail.ru>
2021-12-24 20:50:21 +0300

    2 task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,14 @@
 function filterRange(arr, a, b) {
   // ваш код...
+  //     let filterArray = [];
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] >= a && arr[i] <= b){
+//           filterArray.push(arr[i]);
+//         }
+//     }
+//     return filterArray;
+// }
+return arr.filter( function(element){
+  return (element >= a && element <= b);
+});
 }
