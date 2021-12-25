a38a5ade270ee05864b39d59052a9fa65eacf34f
Retover <lehasazonoff@mail.ru>
2021-12-25 17:32:01 +0300

    3 task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
   // ваш код...
+  let strArr = str.split("-");
+  let newStr =  strArr.map(function(elem,index){
+      if (index == 0) { 
+          return elem;
+        } else {
+            return elem[0].toUpperCase() + elem.slice(1);
+        }
+    });
+     return newStr.join("")
 }
