ba0105f859d9a3f3aef95244025da682431d4ba1
Retover <lehasazonoff@mail.ru>
2021-12-25 20:00:26 +0300

    4 task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
   // ваш код...
+  let showList = '';
+    for (let i = 0; i < users.length; i++){
+        let user = users[i];
+        if(user.age <= age){
+            showList +=  user.name + ", " + user.balance + "\n" ;
+        }
+
+    };
+    
+    return showList.slice(0,showList.length -1);
 }
