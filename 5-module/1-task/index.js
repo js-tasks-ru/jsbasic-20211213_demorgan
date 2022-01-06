f0b62e6e6b892e701e56d5e2d20053d5094e90bd
Retover <lehasazonoff@mail.ru>
2022-01-06 22:48:34 +0300

    1 task

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,8 @@
 function hideSelf() {
   // ваш код...
+ let button = document.getElementsByClassName('hide-self-button');
+ //button[0].setAttribute('onclick','this.hidden=true');
+ button[0].onclick = function(){
+   this.hidden = true;
+ }
 }
