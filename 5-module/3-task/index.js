3ed0b4f9855292346978101659e73189b2874cc6
Retover <lehasazonoff@mail.ru>
2022-01-07 20:06:33 +0300

    3 task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,25 @@
 function initCarousel() {
   // ваш код...
-}
+  let buttonRight = document.querySelector('.carousel__arrow_right');
+  let buttonLeft = document.querySelector('.carousel__arrow_left');
+  let karusel = document.querySelector('.carousel__inner');
+  let y = document.querySelector('.carousel__inner').offsetWidth;
+  let x = 0;
+  buttonLeft.style.display = 'none';
+  buttonRight.onclick = function(){
+    x -= y;
+    karusel.style.transform = `translateX(${x}px)`;
+    buttonLeft.style.display = '';
+    if(x == -y * 3){
+      buttonRight.style.display = 'none';
+    }
+  }
+  buttonLeft.onclick = function(){
+    x +=y;
+    karusel.style.transform = `translateX(${x}px)`
+    buttonRight.style.display = '';
+    if(x == 0){
+      buttonLeft.style.display = 'none';
+    }
+   }
+ }
